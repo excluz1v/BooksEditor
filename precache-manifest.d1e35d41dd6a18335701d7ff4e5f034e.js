89c182e141310411a91411df2fc10304d2efe069
excluz1v <bmd4work@gmail.com>
2020-02-07 00:06:07 +0500

    Updates

---
+++ precache-manifest.d1e35d41dd6a18335701d7ff4e5f034e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e78b7f5a0fd20d3dace86f330faa43",
+    "url": "/BooksEditor/index.html"
+  },
+  {
+    "revision": "501977c421711acb234a",
+    "url": "/BooksEditor/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "afe9ec1451de61810fe3",
+    "url": "/BooksEditor/static/css/main.b36544d1.chunk.css"
+  },
+  {
+    "revision": "501977c421711acb234a",
+    "url": "/BooksEditor/static/js/2.35d19996.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/BooksEditor/static/js/2.35d19996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe9ec1451de61810fe3",
+    "url": "/BooksEditor/static/js/main.9cc85a7f.chunk.js"
+  },
+  {
+    "revision": "51cf199120cb7326352a",
+    "url": "/BooksEditor/static/js/runtime-main.c496d11c.js"
+  }
+]);
